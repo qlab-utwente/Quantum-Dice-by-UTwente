cc1da8b7199fab8be32b96ef49409215ee886d56
m7695371 <a.n.vanrossum@utwente.nl>
2025-10-06 15:58:19 +0200

    Update diceConfig.h

--- Arduino/QuantumDice/diceConfig.h
+++ Arduino/QuantumDice/diceConfig.h
@@ -5,15 +5,18 @@
 macAddresses via Tools-Get Board Info or run getMacAddress.ino sketch
 */
 // List of all dice sets
-#define DICE_SET_S000 0  //TEST1
+#define DICE_SET_S000 0  //TEST1 Aernout
 #define DICE_SET_S001 1  //BART1
 #define DICE_SET_S002 2  //BART2
 #define DICE_SET_S003 3  //SQD1
 #define DICE_SET_S004 4  //non existent
 #define DICE_SET_S005 5  //TELEP
+#define DICE_SET_S010 10  //BLCK1 (prehistoric)
+#define DICE_SET_S020 20  //BART3
+#define DICE_SET_S021 21  //BART4
 
 //select one of the above
-#define SELECTED_DICE_SET DICE_SET_S000
+#define SELECTED_DICE_SET DICE_SET_S021
 
 // Configuration for each dice set
 
@@ -48,7 +51,7 @@ inline uint8_t deviceA_mac[6] = { 0xDC, 0xDA, 0x0C, 0x21, 0x06, 0xD8 };  // MAC 
 inline uint8_t deviceB1_mac[6] = { 0x74, 0x4D, 0xBD, 0xA0, 0x3D, 0xE0 };  // MAC address of device B
 inline uint8_t deviceB2_mac[6] = { 0xDC, 0xDA, 0xC, 0x21, 0x2, 0x44 };  // DUMMY. Replace with actual mac adress
 //background color of display
-// use one of the following colors:  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORANGE, GREY, BORDEAUX, DINOGREEN, WHITE
+// use one of the following colors, 0x  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORANGE, GREY, BORDEAUX, DINOGREEN, WHITE
 #define X_BACKGROUND GC9A01A_BLACK
 #define Y_BACKGROUND GC9A01A_BLACK
 #define Z_BACKGROUND GC9A01A_BLACK
@@ -74,7 +77,7 @@ inline uint8_t deviceB2_mac[6] = { 0xDC, 0xDA, 0xC, 0x21, 0x2, 0x44 };  // DUMMY
 // inline uint8_t deviceB1_mac[6] = { 0x74, 0x4D, 0xBD, 0xA1, 0x2C, 0x8C };  // MAC address of device B
 // inline uint8_t deviceB2_mac[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 //background color of display
-// use one of the following colors:  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORNAG, GREY, BORDEAUX, DINOGREEN, WHITE
+// use one of the following colors, 0x  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORNAG, GREY, BORDEAUX, DINOGREEN, WHITE
 #define X_BACKGROUND GC9A01A_BLACK
 #define Y_BACKGROUND GC9A01A_BLACK
 #define Z_BACKGROUND GC9A01A_BLACK
@@ -117,7 +120,7 @@ inline uint8_t deviceA_mac[6] = { 0x3C, 0x84, 0x27, 0xC2, 0xCC, 0x54 };  //SMD
 //force the dices to always produce 7, no matter which axis is measured. Uncomment to ignore
 #define ALWAYS_SEVEN
 
-// use one of the following colors:  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORANGE, GREY, BORDEAUX, DINOGREEN, WHITE
+// use one of the following colors, 0x  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORANGE, GREY, BORDEAUX, DINOGREEN, WHITE
 //background color of display
 #define X_BACKGROUND GC9A01A_BLACK
 #define Y_BACKGROUND GC9A01A_BLACK
@@ -128,6 +131,56 @@ inline uint8_t deviceA_mac[6] = { 0x3C, 0x84, 0x27, 0xC2, 0xCC, 0x54 };  //SMD
 #define RSSILIMIT -35       //RSSI value to detect close by for entanglement. Less negative is less sensitive
 
 //**********************************************//
+
+//ESP32 SMD v3.2 n16r8,
+#elif SELECTED_DICE_SET == DICE_SET_S020
+#define DICE_ID "BART3"
+#define SMD //default SMD. Optional HDR for ancient processor boards
+#define DEVKIT //default DEVKIT. Optional NANO for processorPCB based on Arduino ESP32 nano board
+
+//0xD0, 0xCF, 0x13, 0x33, 0x58, 0x70	
+//0xD0, 0xCF, 0x13, 0x36, 0x40, 0x94
+
+inline uint8_t deviceA_mac[6] = { 0xD0, 0xCF, 0x13, 0x33, 0x58, 0x70 }; // MAC address of device A
+inline uint8_t deviceB1_mac[6] = { 0xD0, 0xCF, 0x13, 0x36, 0x40, 0x94 };  // MAC address of device B
+inline uint8_t deviceB2_mac[6] = { 0xDC, 0xDA, 0xC, 0x21, 0x2, 0x44 };  // DUMMY. Replace with actual mac adress. Use 3rd dice with the teleportation experiment
+
+//background color of display. Select  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORNAG, GREY, BORDEAUX, DINOGREEN, WHITE
+#define X_BACKGROUND GC9A01A_BLACK
+#define Y_BACKGROUND GC9A01A_BLACK
+#define Z_BACKGROUND GC9A01A_BLACK
+#define ENTANG_AB1_COLOR GC9A01A_YELLOW
+#define ENTANG_AB2_COLOR GC9A01A_GREEN
+
+#define RSSILIMIT -35       //RSSI value to detect close by for entanglement. Less negative is less sensitive
+
+//**********************************************//
+
+//ESP32 SMD v3.2 n16r8,
+#elif SELECTED_DICE_SET == DICE_SET_S021
+#define DICE_ID "BART4"
+#define SMD //default SMD. Optional HDR for ancient processor boards
+#define DEVKIT //default DEVKIT. Optional NANO for processorPCB based on Arduino ESP32 nano board
+
+//0xD0, 0xCF, 0x13, 0x36, 0x40, 0x34
+//0xD0, 0xCF, 0x13, 0x33, 0x58, 0x2C
+
+inline uint8_t deviceA_mac[6] = { 0xD0, 0xCF, 0x13, 0x36, 0x40, 0x34 }; // MAC address of device A
+inline uint8_t deviceB1_mac[6] = { 0xD0, 0xCF, 0x13, 0x33, 0x58, 0x2C };  // MAC address of device B
+inline uint8_t deviceB2_mac[6] = { 0xDC, 0xDA, 0xC, 0x21, 0x2, 0x44 };  // DUMMY. Replace with actual mac adress. Use 3rd dice with the teleportation experiment
+
+//background color of display. Select  BLACK, BLUE, RED, GREEN, CYAN, MAGNETA, YELLOW, WHITE, ORNAG, GREY, BORDEAUX, DINOGREEN, WHITE
+#define X_BACKGROUND GC9A01A_BLACK
+#define Y_BACKGROUND GC9A01A_BLACK
+#define Z_BACKGROUND GC9A01A_BLACK
+#define ENTANG_AB1_COLOR GC9A01A_YELLOW
+#define ENTANG_AB2_COLOR GC9A01A_GREEN
+
+#define RSSILIMIT -35       //RSSI value to detect close by for entanglement. Less negative is less sensitive
+
+//**********************************************//
+
+
 
 
 #else
