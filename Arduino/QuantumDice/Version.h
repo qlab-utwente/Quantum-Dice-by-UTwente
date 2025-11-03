e2e6c0896b07cfe58a01193f4e3426a24121e28b
A. van Rossum <36618231+ator1811@users.noreply.github.com>
2025-11-03 10:57:29 +0100

    Update Version.h

--- Arduino/QuantumDice/Version.h
+++ Arduino/QuantumDice/Version.h
@@ -16,5 +16,6 @@
 //#define VERSION "070" //implementation new espnow class from M with esp32 core 3.2.1
 //#define VERSION "071" //bugfixing implementation new espnow class from M with esp32 core 3.3.0
 //#define VERSION "075" //config file in eeprom
-#define VERSION "1.0.0" //release version
+//#define VERSION "1.0.0" //release version
+#define VERSION "1.0.1" //increase stable criterium
 #endif // VERSION_H
